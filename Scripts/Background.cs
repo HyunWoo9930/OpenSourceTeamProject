f853d8bc1c787cb7eaf80a7917ee24f65762af16
HyunWoo9930 <hw62459930@gmail.com>
2024-04-27 21:40:00 +0900

    [ADD] First Commit

---
+++ Scripts/Background.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        debug.log("sda");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
