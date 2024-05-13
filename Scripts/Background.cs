fb0bc21224cf3f66107d73a14323f569f25aade0
HyunWoo9930 <ohw9930@hufs.ac.kr>
2024-05-13 17:51:27 +0900

    [ADD] HP bar 줄어드는 로직 추가

--- Scripts/Background.cs
+++ Scripts/Background.cs
@@ -9,15 +9,9 @@ public class Background : MonoBehaviour
     {
 
     }
-
-    private float moveSpeed = 4f;
     
     void Update()
     {
-        transform.position += Vector3.down * moveSpeed * Time.deltaTime;
-        if (transform.position.y < -10)
-        {
-            transform.position += new Vector3(0, 10 * 2f, 0);
-        }
+        
     }
 }
