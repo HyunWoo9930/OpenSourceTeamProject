4ecc8fe57def36c1b2bf178e3a11922da8eeb5a3
wnswo22 <163493979+wnswo22@users.noreply.github.com>
2024-05-29 13:44:59 +0900

    Update Player.cs

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,16 +1,62 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
-    private float moveSpeed;
-    void Update()
-    {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxisRaw("Vertical");
-        Vector3 moveTo = new Vector3(horizontalInput, verticalInput, 0);
-        transform.position += moveTo * moveSpeed * Time.deltaTime;
+    public Animator anim;
+    public AudioSource[] sound;
+    public GameManager gameManager;
+    public DSLManager dslManager;
+    public bool isleft = true, isDie = false;
+    public int characterIndex, stairIndex, money;
+
+    void Awake() {
+        anim = gameObject.GetComponent<Animator>();
+        money = dslManager.GetMoney();
     }
+
+    public void Climb(bool isChange)
+    {
+        if (isChange) isleft = !isleft;
+        gameManager.StairMove(stairIndex, isChange, isleft);
+        if ((++stairIndex).Equals(20)) stairIndex = 0;
+        MoveAnimation();
+        gameManager.gaugeStart = true;
+    }
+
+
+    public void MoveAnimation()
+    {
+        //Change left and right when changing direction
+        if (!isleft)
+            transform.rotation = Quaternion.Euler(0, -180, 0);
+        else
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        if (isDie) return;
+        anim.SetBool("Move",true);
+        gameManager.PlaySound(1);
+        Invoke("IdleAnimation", 0.05f);        
+    }
+
+    public void IdleAnimation()
+    {
+        anim.SetBool("Move", false);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            collision.gameObject.SetActive(false);
+            gameManager.PlaySound(0);
+            money += 2;
+            dslManager.LoadMoney(money);
+        }
+    }
+
+
+   
 }
