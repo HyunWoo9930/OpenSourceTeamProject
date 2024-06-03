b009902e18cb31e5791cf0fa034579910ce9e894
HyunWoo9930 <ohw9930@hufs.ac.kr>
2024-06-03 21:05:49 +0900

    [ADD] BGM 추가:

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class CharacterManager : MonoBehaviour
 {
