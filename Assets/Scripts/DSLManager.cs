0dd590b28a622f83a584749b739e9fd8916c16a3
HyunWoo9930 <ohw9930@hufs.ac.kr>
2024-06-05 14:47:44 +0900

    [UPDATE] public -> private 변경

--- Assets/Scripts/DSLManager.cs
+++ Assets/Scripts/DSLManager.cs
@@ -45,32 +45,32 @@ public class Information {
 
 
 public class DSLManager : MonoBehaviour {
-    List<Character> Characters = new List<Character>();
-    List<Rankings> Rankings = new List<Rankings>();
-    List<Information> Informs = new List<Information>();
+    private List<Character> _characters = new List<Character>();
+    private List<Rankings> _rankings = new List<Rankings>();
+    private List<Information> _informs = new List<Information>();
 
     public GameManager gameManager;
-    public CharacterManager CharacterManager;
+    public CharacterManager characterManager;
     public Text[] rankingText;
     public Sprite[] characterSprite;
     public Image[] rankCharacterImg;
 
     private void Awake() {
         if (!File.Exists(Application.persistentDataPath + "/Characters.json")) {
-            Characters.Add(new Character("Boo", "Boo", 0, true, true));
-            Characters.Add(new Character("Rapper", "래퍼", 500, false, false));
-            Characters.Add(new Character("Secretary", "비서", 500, false, false));
-            Characters.Add(new Character("Boxer", "복서", 1000, false, false));
-            Characters.Add(new Character("CheerLeader", "치어리더", 1000, false, false));
-            Characters.Add(new Character("Sheriff", "보안관", 2000, false, false));
-            Characters.Add(new Character("Plumber", "배관공", 2000, false, false));
+            _characters.Add(new Character("Boo", "Boo", 0, true, true));
+            _characters.Add(new Character("Rapper", "래퍼", 500, false, false));
+            _characters.Add(new Character("Secretary", "비서", 500, false, false));
+            _characters.Add(new Character("Boxer", "복서", 1000, false, false));
+            _characters.Add(new Character("CheerLeader", "치어리더", 1000, false, false));
+            _characters.Add(new Character("Sheriff", "보안관", 2000, false, false));
+            _characters.Add(new Character("Plumber", "배관공", 2000, false, false));
 
-            Rankings.Add(new Rankings(0, 7));
-            Rankings.Add(new Rankings(0, 7));
-            Rankings.Add(new Rankings(0, 7));
-            Rankings.Add(new Rankings(0, 7));
+            _rankings.Add(new Rankings(0, 7));
+            _rankings.Add(new Rankings(0, 7));
+            _rankings.Add(new Rankings(0, 7));
+            _rankings.Add(new Rankings(0, 7));
 
-            Informs.Add(new Information(0, true, true, true, false));
+            _informs.Add(new Information(0, true, true, true, false));
 
             SaveData();
         }
@@ -85,87 +85,87 @@ public class DSLManager : MonoBehaviour {
     }
     
     public void SaveData() {
-        File.WriteAllText(Application.persistentDataPath + "/Characters.json", Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(Characters))));
-        File.WriteAllText(Application.persistentDataPath + "/Rankings.json", Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(Rankings))));
-        File.WriteAllText(Application.persistentDataPath + "/Informs.json", Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(Informs))));
+        File.WriteAllText(Application.persistentDataPath + "/Characters.json", Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(_characters))));
+        File.WriteAllText(Application.persistentDataPath + "/Rankings.json", Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(_rankings))));
+        File.WriteAllText(Application.persistentDataPath + "/Informs.json", Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(_informs))));
     }
 
 
     private void LoadData() {
-        Characters = JsonConvert.DeserializeObject<List<Character>>(System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(File.ReadAllText(Application.persistentDataPath + "/Characters.json"))));
-        Rankings = JsonConvert.DeserializeObject<List<Rankings>>(System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(File.ReadAllText(Application.persistentDataPath + "/Rankings.json"))));
-        Informs = JsonConvert.DeserializeObject<List<Information>>(System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(File.ReadAllText(Application.persistentDataPath + "/Informs.json"))));
+        _characters = JsonConvert.DeserializeObject<List<Character>>(System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(File.ReadAllText(Application.persistentDataPath + "/Characters.json"))));
+        _rankings = JsonConvert.DeserializeObject<List<Rankings>>(System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(File.ReadAllText(Application.persistentDataPath + "/Rankings.json"))));
+        _informs = JsonConvert.DeserializeObject<List<Information>>(System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(File.ReadAllText(Application.persistentDataPath + "/Informs.json"))));
     }
     
     public void SaveCharacterIndex() {
-        for (int i = 0; i < Characters.Count; i++)
-            Characters[i].Selected = false;
-        Characters[CharacterManager.index].Selected = true;
+        for (int i = 0; i < _characters.Count; i++)
+            _characters[i].Selected = false;
+        _characters[characterManager.index].Selected = true;
         SaveData();
         SceneManager.LoadScene(0);
     }
 
     public int GetSelectedCharIndex() {
         LoadData();
-        for (int i = 0; i < Characters.Count; i++)
-            if (Characters[i].Selected) return i;
+        for (int i = 0; i < _characters.Count; i++)
+            if (_characters[i].Selected) return i;
         return 0;
     }
 
     public bool IsPurchased(int index) {
         LoadData();
-        return Characters[index].Purchased;
+        return _characters[index].Purchased;
     }
 
     public void SaveCharacterPurchased(Animator obj) {
-        if (Characters[CharacterManager.index].Price > Informs[0].Money)
+        if (_characters[characterManager.index].Price > _informs[0].Money)
             obj.GetComponent<Animator>().SetTrigger("notice");
         else {
-            Characters[CharacterManager.index].Purchased = true;
+            _characters[characterManager.index].Purchased = true;
             SaveData();
             LoadData();
-            Informs[0].Money -= Characters[CharacterManager.index].Price;
+            _informs[0].Money -= _characters[characterManager.index].Price;
             SaveData();
-            CharacterManager.ArrowBtn("null");
+            characterManager.ArrowBtn("null");
         }
     }
 
-    public int GetPrice() { return Characters[CharacterManager.index].Price; }
+    public int GetPrice() { return _characters[characterManager.index].Price; }
     
     public int GetMoney() {
         LoadData();
-        return Informs[0].Money;
+        return _informs[0].Money;
     }
 
     public void SaveMoney(int money) {
         LoadData();
-        Informs[0].Money = money;
+        _informs[0].Money = money;
         SaveData();
     }
 
-    public bool IsRetry() { return Informs[0].Retry; }
+    public bool IsRetry() { return _informs[0].Retry; }
 
     public void ChangeRetry(bool isRetry) {
         LoadData();
-        Informs[0].Retry = isRetry;
+        _informs[0].Retry = isRetry;
         SaveData();
     }
     
     private void LoadRanking() {
         for (int i = 0; i < rankingText.Length; i++) {
-            rankingText[i].text = Rankings[i].Score == 0 ? " " : Rankings[i].Score.ToString();
-            rankCharacterImg[i].sprite = characterSprite[Rankings[i].CharacterIndex];
+            rankingText[i].text = _rankings[i].Score == 0 ? " " : _rankings[i].Score.ToString();
+            rankCharacterImg[i].sprite = characterSprite[_rankings[i].CharacterIndex];
         }
     }
 
     public void SaveRankScore(int finalScore) {
-        Rankings[3].Score = finalScore;
+        _rankings[3].Score = finalScore;
         SaveData();
         
         int charIndex = GetSelectedCharIndex();
-        Rankings[3].CharacterIndex = charIndex;
+        _rankings[3].CharacterIndex = charIndex;
         
-        Rankings.Sort(delegate (Rankings a, Rankings b) { return b.Score.CompareTo(a.Score); });
+        _rankings.Sort(delegate (Rankings a, Rankings b) { return b.Score.CompareTo(a.Score); });
 
         SaveData();
         LoadData();
@@ -173,18 +173,18 @@ public class DSLManager : MonoBehaviour {
 
     public int GetBestScore() {
         LoadData();
-        return Rankings[0].Score;
+        return _rankings[0].Score;
     }
     
     public bool GetSettingOn(string type) {
         LoadData();
         switch (type) {
             case "BgmBtn":
-                return Informs[0].BgmOn;
+                return _informs[0].BgmOn;
             case "SoundBtn":
-                return Informs[0].SoundEffectOn;
+                return _informs[0].SoundEffectOn;
             case "VibrateBtn":
-                return Informs[0].VibrationOn;
+                return _informs[0].VibrationOn;
         }
         return false;
     }
@@ -192,13 +192,13 @@ public class DSLManager : MonoBehaviour {
     public void ChangeOnOff(Button btn) {
         LoadData();
         if (btn.name == "BgmBtn") {
-            Informs[0].BgmOn = !Informs[0].BgmOn;
+            _informs[0].BgmOn = !_informs[0].BgmOn;
         }
         if (btn.name == "SoundBtn") {
-            Informs[0].SoundEffectOn = !Informs[0].SoundEffectOn;
+            _informs[0].SoundEffectOn = !_informs[0].SoundEffectOn;
         }
         if (btn.name == "VibrateBtn") {
-            Informs[0].VibrationOn = !Informs[0].VibrationOn;
+            _informs[0].VibrationOn = !_informs[0].VibrationOn;
         }
         SaveData();
         gameManager.SettingOnOff(btn.name);
